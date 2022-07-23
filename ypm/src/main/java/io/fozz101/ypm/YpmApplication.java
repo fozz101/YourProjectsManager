c3539fbe433f04f3bd9ee092c61be944b3c02098
fozz101 <fedi.galfat@esprit.tn>
2022-07-23 14:23:17 +0100

    first commit

---
+++ ypm/src/main/java/io/fozz101/ypm/YpmApplication.java
@@ -0,0 +1,13 @@
+package io.fozz101.ypm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YpmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YpmApplication.class, args);
+	}
+
+}
---
+++ ypm/src/test/java/io/fozz101/ypm/YpmApplicationTests.java
@@ -0,0 +1,13 @@
+package io.fozz101.ypm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YpmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
