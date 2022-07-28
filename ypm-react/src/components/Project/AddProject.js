516184b90044e2a174601f2c34a819dd461dfba8
fozz101 <fedi.galfat@esprit.tn>
2022-07-28 18:21:14 +0100

    Get form validation errors from Redux

--- ypm-react/src/components/Project/AddProject.js
+++ ypm-react/src/components/Project/AddProject.js
@@ -11,11 +11,21 @@ class AddProject extends Component {
             projectIdentifier: "",
             description: "",
             start_date: "",
-            end_date: ""
+            end_date: "",
+            errors: {}
         };
         this.onChange = this.onChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
     }
+
+    //life cycle hooks
+    componentWillReceiveProps(nextProps){
+        if (nextProps.errors){
+            this.setState({errors:nextProps.errors});
+        }
+    }
+
+
     onChange(e){
         this.setState({[e.target.name]:e.target.value})
     }
@@ -32,8 +42,9 @@ class AddProject extends Component {
     }
 
   render() {
+    const {errors} = this.state
     return (
-
+    <div>
       <div className="project">
         <div className="container">
             <div className="row">
@@ -43,14 +54,17 @@ class AddProject extends Component {
                     <form onSubmit={this.onSubmit}>
                         <div className="form-group">
                             <input type="text" className="form-control form-control-lg " placeholder="Project Name" name="projectName" value={this.state.projectName} onChange={this.onChange}/>
-                        </div>
+                            <p>{errors.projectName}</p>
+                            </div>
                         <div className="form-group">
                             <input type="text" className="form-control form-control-lg" placeholder="Unique Project ID" name='projectIdentifier' value={this.state.projectIdentifier}
                             onChange={this.onChange} />
+                            <p>{errors.projectIdentifier}</p>
                         </div>
                         <div className="form-group">
                             <textarea className="form-control form-control-lg" placeholder="Project Description" name='description' value={this.state.description} onChange={this.onChange}></textarea>
-                        </div>
+                            <p>{errors.description}</p>
+                            </div>
                         <h6>Start Date</h6>
                         <div className="form-group">
                             <input type="date" className="form-control form-control-lg" name="start_date" value={this.state.start_date} onChange={this.onChange} />
@@ -66,6 +80,7 @@ class AddProject extends Component {
             </div>
         </div>
     </div>
+    </div>
 
       
       
@@ -76,10 +91,15 @@ class AddProject extends Component {
 }
 
 AddProject.propTypes = {
-    createProject: PropTypes.func.isRequired
+    createProject: PropTypes.func.isRequired,
+    errors: PropTypes.object.isRequired
   };
+
+  const mapStateToProps = state =>({
+    errors: state.errors
+  });
   
   export default connect(
-    null,
+    mapStateToProps,
     { createProject }
   )(AddProject);
