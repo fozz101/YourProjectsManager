72b4cc18cb4ca1fd47d7cf187760f33da3f41343
fozz101 <fedi.galfat@esprit.tn>
2022-07-31 19:14:33 +0100

    Load Project Task to state

--- ypm-react/src/actions/backlogActions.js
+++ ypm-react/src/actions/backlogActions.js
@@ -17,3 +17,15 @@ export const addProjectTask = (backlog_id, project_task, history) => async dispa
 
 
 };
+
+export const getBacklog =(backlog_id) => async dispatch => {
+    try {
+        const res = await axios.get(`/api/backlog/${backlog_id}`)
+        dispatch({
+            type:GET_BACKLOG,
+            payload: res.data
+        })
+    } catch (error) {
+        
+    }
+}
