da0f990199b78e843913dd5ebe364eced280b9a5
fozz101 <fedi.galfat@esprit.tn>
2022-08-07 02:52:50 +0100

    Handle login form with jwtToken and log out

--- ypm-react/src/actions/securityActions.js
+++ ypm-react/src/actions/securityActions.js
@@ -22,26 +22,37 @@ export const createNewUser = (newUser, history) =>async dispatch => {
 
 export const login = LoginRequest => async dispatch => {
     try {
-    // post login request
-    const res = await axios.post("/api/user/login", LoginRequest);
-    //extract token from res.data
-    const {token} = res.data;
-    //store the token in the localStorage
-    localStorage.setItem("jwtToken",token);
-    //Set our token in header ***
-    setJWToken(token);
-    // decode token on React
-    const decoded = jwt_decode(token);
-    // dispatch to securityReducer
-    dispatch ({
-        type:SET_CURRENT_USER,
-        payload : decoded
-    })
-    } catch (error) {
-        dispatch ({
-            type:GET_ERRORS,
-            payload : error.response.data
-        })
+      // post => Login Request
+      const res = await axios.post("/api/users/login", LoginRequest);
+      // extract token from res.data
+      const { token } = res.data;
+      // store the token in the localStorage
+      localStorage.setItem("jwtToken", token);
+      // set our token in header ***
+      setJWToken(token);
+      // decode token on React
+      const decoded = jwt_decode(token);
+      // dispatch to our securityReducer
+      dispatch({
+        type: SET_CURRENT_USER,
+        payload: decoded
+      });
+    } catch (err) {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      });
     }
+  };
 
-}
+
+  export const logout = () => dispatch => {
+    localStorage.removeItem("jwtToken");
+    setJWToken(false);
+    dispatch({
+        type: SET_CURRENT_USER,
+        payload: {}
+      });
+    
+
+  };
