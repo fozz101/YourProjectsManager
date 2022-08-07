4209ae27fec4facd3f20ddbe9b4355d32abd4b67
fozz101 <fedi.galfat@esprit.tn>
2022-08-07 01:33:33 +0100

    Implemented securityActions and securityReducer

--- ypm-react/src/actions/securityActions.js
+++ ypm-react/src/actions/securityActions.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import setJWToken from "../securityUtils/setJWToken";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import jwt_decode from "jwt-decode"; 
 
 export const createNewUser = (newUser, history) =>async dispatch => {
     try {
@@ -16,4 +18,30 @@ export const createNewUser = (newUser, history) =>async dispatch => {
             payload: error.response.data
         });     
     }
+}
+
+export const login = LoginRequest => async dispatch => {
+    try {
+    // post login request
+    const res = await axios.post("/api/user/login", LoginRequest);
+    //extract token from res.data
+    const {token} = res.data;
+    //store the token in the localStorage
+    localStorage.setItem("jwtToken",token);
+    //Set our token in header ***
+    setJWToken(token);
+    // decode token on React
+    const decoded = jwt_decode(token);
+    // dispatch to securityReducer
+    dispatch ({
+        type:SET_CURRENT_USER,
+        payload : decoded
+    })
+    } catch (error) {
+        dispatch ({
+            type:GET_ERRORS,
+            payload : error.response.data
+        })
+    }
+
 }
